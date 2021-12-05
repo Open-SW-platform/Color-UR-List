59ef5a0885a5ad0495bea10ff81ea5417de716c2
Hyuna Kim <akxmcse@gmail.com>
2021-12-06 03:09:53 +0900

    <feat> 달성률 today 탭 상단에 Today 컴포넌트 추가

--- src/screens/DayScreen.js
+++ src/screens/DayScreen.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import { images } from '../images';
-import IconButton from '../components/IconButton';
-import {viewStyles, textStyles, barStyles} from '../styles'
+import { View, StatusBar } from 'react-native';
+import {Container, barStyles, viewStyles} from '../styles'
+import {ThemeProvider} from 'styled-components/native';
+import {theme} from '../theme';
+
+
 import Today from '../components/Today';
 
 export default function DayScreen() {
+
+  var TopBar;
+
+  TopBar =
+  <View style={viewStyles.settingView} >
+    <Today />
+  </View>
+
     return (
-  <Text>DAY</Text>
+      <ThemeProvider theme= {theme}>
+      <Container>
+        <StatusBar barStyle="light-content" style={barStyles.statusBar} />
+        {TopBar}
+      </Container>
+      </ThemeProvider>
     );
 }
-
 
