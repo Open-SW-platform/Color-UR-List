75bd518571cecf59a2d86b6389f24290eaa57618
Hyuna Kim <akxmcse@gmail.com>
2021-12-06 19:12:49 +0900

    <chore> day tab Git 동기화 관련 간단한 에러 해결

--- src/screens/DayScreen.js
+++ src/screens/DayScreen.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
-import {Container, barStyles, viewStyles} from '../styles'
-import {ThemeProvider} from 'styled-components/native';
-import {theme} from '../theme';
+import { Text, View, StatusBar, ScrollView } from 'react-native';
+import { Container, barStyles, viewStyles } from '../styles'
+import { ThemeProvider } from 'styled-components/native';
+import { theme } from '../theme';
+import { ProgressChart } from 'react-native-chart-kit';
+import * as Progress from 'react-native-progress';
 
 
 import Today from '../components/Today';
 
 export default function DayScreen() {
 
-  var TopBar =
-  <View style={viewStyles.settingView} >
-    <Today />
-  </View>
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
 
-    return (
-      <ThemeProvider theme= {theme}>
+  const data = {
+    data: [0.4]
+  };
+
+  var TopBar =
+    <View style={viewStyles.settingView} >
+      <Today />
+    </View>
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" style={barStyles.statusBar} />
+      {TopBar}
       <Container>
-<<<<<<< Updated upstream
-        <StatusBar barStyle="light-content" style={barStyles.statusBar} />
-        {TopBar}
-=======
         <ProgressChart
           style={{ paddingTop: 20, flex: 1 }}
           data={data}
@@ -31,12 +46,10 @@ export default function DayScreen() {
           chartConfig={chartConfig}
           hideLegend={true}
         />
-
-        
         <Container>
           <ScrollView width={300}>
             <Text style={{ padding: 10, color: 'white' }}>Study</Text>
-            <Progress.Bar progress={0.3} width={250} /> 
+            <Progress.Bar progress={0.3} width={250} />
             <Text style={{ padding: 10, color: 'white' }}> 30% </Text>
 
             <Text style={{ padding: 10, color: 'white' }}>Work</Text>
@@ -52,10 +65,8 @@ export default function DayScreen() {
             <Text style={{ padding: 10, color: 'white' }}> 30% </Text>
           </ScrollView>
         </Container>
-
->>>>>>> Stashed changes
       </Container>
-      </ThemeProvider>
-    );
+    </ThemeProvider>
+  );
 }
 
