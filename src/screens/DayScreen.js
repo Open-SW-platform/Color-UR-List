c6839e676130868995164d90d4774adac796d33a
Hyuna Kim <akxmcse@gmail.com>
2021-12-19 15:37:44 +0900

    <layout> day tab total 성취도 요소 추가
        "react-native-svg": "12.1.1" 모듈 설치하였습니다

--- src/screens/DayScreen.js
+++ src/screens/DayScreen.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react';
+import React, { useState, useContext } from 'react';
 import { Text, View, StatusBar, ScrollView } from 'react-native';
 import { textStyles, Container, barStyles, viewStyles } from '../styles'
 import { ThemeProvider } from 'styled-components/native';
@@ -7,6 +7,7 @@ import { ProgressChart } from 'react-native-chart-kit';
 import * as Progress from 'react-native-progress';
 import Today from '../components/Today';
 import TaskContext from '../contexts/Tasks';
+import Svg, { Line } from 'react-native-svg';
 
 export default function DayScreen() {
 
@@ -58,10 +59,10 @@ export default function DayScreen() {
       <StatusBar barStyle="light-content" style={barStyles.statusBar} />
       {TopBar}
       <Container>
-        <Text style={{ fontSize: 30, position: 'absolute', zIndex: 2, marginTop: 90, textAlign:'center'}}> Do {completed} tasks {"\n"} today </Text>
-        <Text style={{ fontSize: 20, position: 'absolute', zIndex: 2, marginTop: 160, textAlign:'center'}}> Your goal is to {"\n"} complete {total} tasks </Text>
+        <Text style={{ fontSize: 30, position: 'absolute', zIndex: 2, marginTop: 80, textAlign: 'center' }}> Do {completed} tasks {"\n"} today </Text>
+        <Text style={{ fontSize: 20, position: 'absolute', zIndex: 2, marginTop: 150, textAlign: 'center' }}> Your goal is to {"\n"} complete {total} tasks </Text>
         <ProgressChart
-          style={{ paddingTop: 20, flex: 1, position: 'absolute', zIndex: 1 }}
+          style={{ paddingTop: 10, flex: 1, position: 'absolute', zIndex: 1 }}
           data={progressChartData}
           width={400}
           height={250}
@@ -71,32 +72,53 @@ export default function DayScreen() {
           hideLegend={true}
         />
 
+        <Svg height="1000" width="1000" style={{ position: 'absolute', zIndex: 3 }}>
+          <Line x1="0" y1="290" x2="1000" y2="290" stroke="#e0e0e0" strokeWidth="2" />
+        </Svg>
 
+        <Svg height="1000" width="1000" style={{ position: 'absolute', zIndex: 3 }}>
+          <Line x1="0" y1="380" x2="1000" y2="380" stroke="#e0e0e0" strokeWidth="2" />
+        </Svg>
 
-        <Container style={{marginTop: 400}}>
-          <ScrollView width={350} >
-            <Text style={{ fontSize: 20, paddingLeft: 15, color: 'black' }}>Study</Text>
+        <Container style={{ marginTop: 310}}>
+          <View style={{ flexDirection: 'row'}} >
+            <View style={{ marginHorizontal: 20 }}>
+              <Text style={{ fontSize: 20, textAlign: 'center' }}>Total task</Text>
+              <Text style={{ fontSize: 30, textAlign: 'center' }}>{total}</Text>
+            </View>
+            <View style={{ marginHorizontal: 20 }}>
+              <Text style={{ fontSize: 20, textAlign: 'center' }}>Completed</Text>
+              <Text style={{ fontSize: 30, textAlign: 'center' }}>{completed}</Text>
+            </View>
+            <View style={{ marginHorizontal: 25 }}>
+              <Text style={{ fontSize: 20, textAlign: 'center' }}>Rate</Text>
+              <Text style={{ fontSize: 30, textAlign: 'center' }}>{completedPercentage}%</Text>
+            </View>
+          </View>
+          
+          <ScrollView width={350} style={{ marginTop: 50}} >
+            <Text style={{ fontSize: 20, paddingLeft: 10, color: 'black' }}>Study</Text>
             <View style={{ flexDirection: 'row' }} >
-              <Progress.Bar progress={completedPercentageStudy / 100} width={270} height={10} color= {themeColor} style={{ margin: 15 }} />
-              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {completedStudy}/{totalStudy} </Text>
+              <Progress.Bar progress={completedPercentageStudy / 100} width={270} height={10} color={themeColor} style={{ margin: 10 }} />
+              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {completedPercentageStudy}% </Text>
             </View>
 
-            <Text style={{ fontSize: 20, paddingLeft: 15, paddingTop: 10, color: 'black' }}>Work</Text>
+            <Text style={{ fontSize: 20, paddingLeft: 10, paddingTop: 10, color: 'black' }}>Work</Text>
             <View style={{ flexDirection: 'row' }} >
-              <Progress.Bar progress={completedPercentageWork / 100} width={270} height={10} color= {themeColor} style={{ margin: 15 }} />
-              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {completedWork}/{totalWork} </Text>
+              <Progress.Bar progress={completedPercentageWork / 100} width={270} height={10} color={themeColor} style={{ margin: 10 }} />
+              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {completedPercentageWork}% </Text>
             </View>
 
-            <Text style={{ fontSize: 20, paddingLeft: 15, paddingTop: 10, color: 'black' }}>Exercise</Text>
+            <Text style={{ fontSize: 20, paddingLeft: 10, paddingTop: 10, color: 'black' }}>Exercise</Text>
             <View style={{ flexDirection: 'row' }} >
-              <Progress.Bar progress={completedPercentageExercise / 100} width={270} height={10} color= {themeColor} style={{ margin : 15 }} />
-              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {completedExercise}/{totalWork} </Text>
+              <Progress.Bar progress={completedPercentageExercise / 100} width={270} height={10} color={themeColor} style={{ margin: 10 }} />
+              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {completedPercentageExercise}% </Text>
             </View>
 
-            <Text style={{ fontSize: 20, paddingLeft: 15, paddingTop: 10, color: 'black' }}>Assignment</Text>
+            <Text style={{ fontSize: 20, paddingLeft: 10, paddingTop: 10, color: 'black' }}>Assignment</Text>
             <View style={{ flexDirection: 'row' }} >
-              <Progress.Bar progress={completedPercentageAssginment / 100} width={270} height={10} color= {themeColor} style={{ margin: 15 }} />
-              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {completedAssginment}/{totalAssginment} </Text>
+              <Progress.Bar progress={completedPercentageAssginment / 100} width={270} height={10} color={themeColor} style={{ margin: 10 }} />
+              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {completedPercentageAssginment}% </Text>
             </View>
           </ScrollView>
         </Container>
