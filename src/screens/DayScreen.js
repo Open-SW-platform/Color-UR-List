0f9a66419140c93e3ff194cf7250a26e786a85b6
Hyuna Kim <akxmcse@gmail.com>
2021-12-19 14:35:06 +0900

    <layout> day tab total task 안내 text 변경

--- src/screens/DayScreen.js
+++ src/screens/DayScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import { Text, View, StatusBar, ScrollView } from 'react-native';
 import { textStyles, Container, barStyles, viewStyles } from '../styles'
 import { ThemeProvider } from 'styled-components/native';
@@ -6,10 +6,11 @@ import { theme } from '../theme';
 import { ProgressChart } from 'react-native-chart-kit';
 import * as Progress from 'react-native-progress';
 import Today from '../components/Today';
-import { useContext } from 'react';
 import TaskContext from '../contexts/Tasks';
 
 export default function DayScreen() {
+
+  const [themeColor, setThemeColor] = useState('#f9ceee');
 
   const chartConfig = {
     backgroundGradientFrom: "#ffffff",
@@ -24,26 +25,26 @@ export default function DayScreen() {
 
   const totalStudy = Object.values(tasks).filter(item => item.category === 0).length
   const completedStudy = Object.values(tasks).filter(item => ((item.category === 0) && (item.completed === true))).length
-  const percentageStudy = Math.floor((completedStudy * 100) / (totalStudy))
+  const completedPercentageStudy = Math.floor((completedStudy * 100) / (totalStudy))
 
   const totalWork = Object.values(tasks).filter(item => item.category === 1).length
   const completedWork = Object.values(tasks).filter(item => ((item.category === 1) && (item.completed === true))).length
-  const percentageWork = Math.floor((completedWork * 100) / (totalWork))
+  const completedPercentageWork = Math.floor((completedWork * 100) / (totalWork))
 
   const totalExercise = Object.values(tasks).filter(item => item.category === 3).length
   const completedExercise = Object.values(tasks).filter(item => ((item.category === 3) && (item.completed === true))).length
-  const percentageExercise = Math.floor((completedExercise * 100) / (totalExercise))
+  const completedPercentageExercise = Math.floor((completedExercise * 100) / (totalExercise))
 
   const totalAssginment = Object.values(tasks).filter(item => item.category === 2).length
   const completedAssginment = Object.values(tasks).filter(item => ((item.category === 2) && (item.completed === true))).length
-  const percentageAssginment = Math.floor((completedAssginment * 100) / (totalAssginment))
+  const completedPercentageAssginment = Math.floor((completedAssginment * 100) / (totalAssginment))
 
   const total = totalStudy + totalWork + totalExercise + totalAssginment
   const completed = completedStudy + completedWork + completedExercise + completedAssginment
-  const percentage = Math.floor((completed * 100) / (total));
+  const completedPercentage = Math.floor((completed * 100) / (total));
 
   var progressChartData = {
-    data: [percentage / 100]
+    data: [completedPercentage / 100]
   };
 
   var TopBar =
@@ -57,7 +58,8 @@ export default function DayScreen() {
       <StatusBar barStyle="light-content" style={barStyles.statusBar} />
       {TopBar}
       <Container>
-        <Text style={{ fontSize: 40, position: 'absolute', zIndex: 2, marginTop: 120}}> {percentage}% </Text>
+        <Text style={{ fontSize: 30, position: 'absolute', zIndex: 2, marginTop: 90, textAlign:'center'}}> Do {completed} tasks {"\n"} today </Text>
+        <Text style={{ fontSize: 20, position: 'absolute', zIndex: 2, marginTop: 160, textAlign:'center'}}> Your goal is to {"\n"} complete {total} tasks </Text>
         <ProgressChart
           style={{ paddingTop: 20, flex: 1, position: 'absolute', zIndex: 1 }}
           data={progressChartData}
@@ -72,26 +74,26 @@ export default function DayScreen() {
           <ScrollView width={350} >
             <Text style={{ fontSize: 20, paddingLeft: 10, color: 'black' }}>Study</Text>
             <View style={{ flexDirection: 'row' }} >
-              <Progress.Bar progress={percentageStudy / 100} width={270} height={10} style={{ margin: 10 }} />
-              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {percentageStudy}% </Text>
+              <Progress.Bar progress={completedPercentageStudy / 100} width={270} height={10} color= {themeColor} style={{ margin: 10 }} />
+              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {completedPercentageStudy}% </Text>
             </View>
 
             <Text style={{ fontSize: 20, paddingLeft: 10, paddingTop: 10, color: 'black' }}>Work</Text>
             <View style={{ flexDirection: 'row' }} >
-              <Progress.Bar progress={percentageWork / 100} width={270} height={10} style={{ margin: 10 }} />
-              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {percentageWork}% </Text>
+              <Progress.Bar progress={completedPercentageWork / 100} width={270} height={10} color= {themeColor} style={{ margin: 10 }} />
+              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {completedPercentageWork}% </Text>
             </View>
 
             <Text style={{ fontSize: 20, paddingLeft: 10, paddingTop: 10, color: 'black' }}>Exercise</Text>
             <View style={{ flexDirection: 'row' }} >
-              <Progress.Bar progress={percentageExercise / 100} width={270} height={10} style={{ margin: 10 }} />
-              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {percentageExercise}% </Text>
+              <Progress.Bar progress={completedPercentageExercise / 100} width={270} height={10} color= {themeColor} style={{ margin: 10 }} />
+              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {completedPercentageExercise}% </Text>
             </View>
 
             <Text style={{ fontSize: 20, paddingLeft: 10, paddingTop: 10, color: 'black' }}>Assignment</Text>
             <View style={{ flexDirection: 'row' }} >
-              <Progress.Bar progress={percentageAssginment / 100} width={270} height={10} style={{ margin: 10 }} />
-              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {percentageAssginment}% </Text>
+              <Progress.Bar progress={completedPercentageAssginment / 100} width={270} height={10} color= {themeColor} style={{ margin: 10 }} />
+              <Text style={{ fontSize: 20, margin: 5, color: 'black' }}> {completedPercentageAssginment}% </Text>
             </View>
           </ScrollView>
         </Container>
